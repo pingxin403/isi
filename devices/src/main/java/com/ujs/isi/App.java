1b98599b7f0e1d511bbbbb5baec3fd8b056074b5
pingxin <m13839441583@163.com>
2019-05-14 19:18:10 +0800

    初始化

---
+++ devices/src/main/java/com/ujs/isi/App.java
@@ -0,0 +1,13 @@
+package com.ujs.isi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
